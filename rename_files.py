15f66bcfcf874520f1c01df2923ce697793c065c
Osama Abou Hajar <smsm.sy@hotmail.com>
2021-10-16 23:51:01 +0100

    fix one file at time

--- rename_files.py
+++ rename_files.py
@@ -31,6 +31,7 @@ AR_EN_ALPHA = {
     'ز':'z', 
     'و':'w', 
     'ر':'r',
+    ' ': ' '
     }
 
 EN_ALPH_LETTER = ['a', 'b', 'c', 'd', 'e', 'f', 
@@ -51,12 +52,14 @@ MENU = '''
        3- Read The App Description.
        '''
 
-path = '/Users/oabouhajar/project/rename_files/files/'
+# TODO: make generic file path [args maybe?]
+path = '/Users/oabouhajar/project/rename_files/Arabic_English__Filename_Renamer/files/'
 files = os.listdir(path)
 
 
 def replace_name(name):
     en_name = ""
+
     for char in name:
         if(char in AR_EN_ALPHA): # handeling Arabic letter replacement
             en_name += AR_EN_ALPHA.get(char)
@@ -70,20 +73,45 @@ def replace_name(name):
     # return english new name
     return en_name
 
-
-def en_ar_replacement():
-    for f in files:
-        file_name , ext= f.split(".")
-        print(file_name)
-        print(ext)
-        new_name = replace_name(file_name)
-        os.rename(path+f, ( path+ new_name + "." + ext))
+def file_name_ext_constractor(path, name, ext):
+    return path+ name + "." + ext
 
 
-def get_user_description():
+def file_renamer(old_file_name, new_file_name , ext):
+    os.rename(file_name_ext_constractor(path,old_file_name,ext),file_name_ext_constractor(path,new_file_name,ext))
+    return True
+
+def en_ar_replacement_process(user_select = '1'):
+    one_at_time = False
+    all_at_one = False
+
+    if(user_select == '1'):
+        all_at_one = True
+    elif user_select == '2':
+        one_at_time = True
+
+    for file in files:
+        old_file_name , ext = file.split(".")
+        new_file_name = replace_name(old_file_name.lower())
+        print("File Extension: \t\t\t",ext)
+        print("Old File Name: \t\t\t", old_file_name)
+        print("New File Name: \t\t\t", new_file_name)
+        if (one_at_time):
+            if(input('''Are you Happy with the generated name [y/n]?''') == 'y'):
+                file_renamer(old_file_name, new_file_name, ext)
+            else:
+                file_renamer(old_file_name, input("Enter the name you like please: "), ext)
+        elif (all_at_one):
+            file_renamer(old_file_name, new_file_name, ext)
+
+
+
+def get_user_selection():
     print(MENU)
     return input("Please Select One OF The Option: ")
 
 
 if __name__ == "__main__":
-    user_select = get_user_description()
+    
+    user_select = get_user_selection()
+    en_ar_replacement_process(user_select)
